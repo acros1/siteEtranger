3b0b54f24bc777aa3f7e085042ca1cfa79a3b264
Alex <Alex@ALEX-PC>
2018-10-03 12:07:01 +0200

    Modification de la page d'acceuil, du header et du cadre image (centrage + changement d'images)

--- public_html/js/caroussel.js
+++ public_html/js/caroussel.js
@@ -2,7 +2,7 @@ var rotate = false;
 
 function setbackground(){
     window.setTimeout( "setbackground()", 3000);
-    newImage = rotate ? 'url(./img/index.jpeg)' : 'url(./img/index2.jpeg)';
+    newImage = rotate ? 'url(./img/batman1.jpg)' : 'url(./img/batman2.jpg)';
     rotate = !rotate;
     document.getElementById("container1").style.backgroundImage = newImage;
 }
