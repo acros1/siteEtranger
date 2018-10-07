8231ef86392cc4193c87b79191d6599553bb542b
Aurelien Michaud <aurelien@debian.home>
2018-10-07 21:36:18 +0200

    Modification of the caroussel, adding pictures, adding information box on top of the caroussel

--- public_html/js/caroussel.js
+++ public_html/js/caroussel.js
@@ -1,10 +1,27 @@
-var rotate = false;
+/* All images names ( not paths, it will be added later ) */
+var imgs	= ["student.jpg",
+		   "map-globe-network.jpg",
+		   "batman1.jpg"];
+/* Index for browsing image and content arrays */
+var i		= 0;
+/* Time to wait until it switch to the next image */
+var pause	= 5000;
+
+/* content for each image, composed of a title and a text */
+var content	= [["Information", "Hey there ! This is where you're going to put all your information about foreigners and the steps you need to follow in order to study abroad !"],
+		   ["Other title", "other text"],
+		   ["Another title", "Another text"]];
 
 function setbackground(){
-    window.setTimeout( "setbackground()", 3000);
-    newImage = rotate ? 'url(./img/batman1.jpg)' : 'url(./img/batman2.jpg)';
-    rotate = !rotate;
-    document.getElementById("container1").style.backgroundImage = newImage;
-}
-
+    window.setTimeout( "setbackground()", pause);
     
+    if(i == imgs.length) {i = 0;}
+    
+    newImage = 'url(./img/' + imgs[i]  + ')';
+
+    document.getElementById("content-index-page").style.backgroundImage = newImage;
+    document.getElementById("info-box-title").innerHTML			= content[i][0];
+    document.getElementById("info-box-text").innerHTML			= content[i][1];
+    
+    i++;
+}
