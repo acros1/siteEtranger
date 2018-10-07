ba4065b58b3cbd88cdfce387f13ab584d79ca18a
Aurelien Michaud <aurelien@debian.home>
2018-10-07 22:07:16 +0200

    Modification sur le caroussel

--- public_html/js/caroussel.js
+++ public_html/js/caroussel.js
@@ -1,7 +1,8 @@
 /* All images names ( not paths, it will be added later ) */
 var imgs	= ["student.jpg",
 		   "map-globe-network.jpg",
-		   "batman1.jpg"];
+		   "passeport.jpg",
+		   "passeport2.jpg"];
 /* Index for browsing image and content arrays */
 var i		= 0;
 /* Time to wait until it switch to the next image */
@@ -10,7 +11,8 @@ var pause	= 5000;
 /* content for each image, composed of a title and a text */
 var content	= [["Information", "Hey there ! This is where you're going to put all your information about foreigners and the steps you need to follow in order to study abroad !"],
 		   ["Other title", "other text"],
-		   ["Another title", "Another text"]];
+		   ["Passeport 1", "text pour image passeport 1"],
+		   ["Passeport 2", "Text pour passeport 2"]];
 
 function setbackground(){
     window.setTimeout( "setbackground()", pause);
