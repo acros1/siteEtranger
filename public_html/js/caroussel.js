12078a4e2169dffacb72bf388ac5fc12a4af2b53
Laura <lauraburlonroux@hotmail.fr>
2018-10-18 23:11:14 +0200

    Modif texte blocs sur page d'accueil

--- public_html/js/caroussel.js
+++ public_html/js/caroussel.js
@@ -5,12 +5,12 @@ var imgs	= ["student.jpg",
 /* Index for browsing image and content arrays */
 var i		= 0;
 /* Time to wait until it switch to the next image */
-var pause	= 5000;
+var pause	= 4000;
 
 /* content for each image, composed of a title and a text */
-var content	= [["Information", "Hey there ! This is where you're going to put all your information about foreigners and the steps you need to follow in order to study abroad !"],
-		   ["Other title", "other text"],
-		   ["Passeport 1", "text pour image passeport 1"]];
+var content	= [["Doubles diplômes", "Envie d'une offre de formation unique vous permettant d'obtenir à la fois le diplôme de l'INSA Toulouse, et celui d'une école partenaire ? </br></br> Venez consulter dès à présent les partenariats possibles en France, mais aussi dans le monde entier ! "],
+		   ["Semestres à l'étranger", "Quelle université choisir pour effctuer un semestre à l'étranger ? Dans quelle langue sont dispensés les cours ? Quelles matières sont enseignées ?</br></br> Dans cette section, retrouvez toutes les informations concernant la scolarité au sein des universités paternaires."],
+		   ["Administration", "Problème de budget, de demande de VISA ou de logement ? Un simple besoin de renseignements ? </br></br> N'hésitez pas à prendre rendez-vous avec le responsable ressources internationales de votre département d'étude ou d'envoyer vos questions via le forumlaire de contact. "]];
 
 function setbackground(){
     window.setTimeout( "setbackground()", pause);
