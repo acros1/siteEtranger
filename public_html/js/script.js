df61c726d3cca1b7ec4617ef462616f91dcd8b97
M. Michaud Aurelien <a_michau@insa-10227>
2018-11-07 18:04:35 +0100

    Commencement map contenu

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -9,3 +9,16 @@ function toggle(div)
 	document.getElementById(div).style.display =  "none";
     }
 }
+
+function toggleMapContent(div)
+{
+    alert("Hello");
+    if(document.getElementById(div).style.display == "none")
+    {
+	document.getElementById(div).style.display =  "block";
+    }
+    else
+    {
+	document.getElementById(div).style.display =  "none";
+    }
+}
