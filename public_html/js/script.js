27f29beb43bd146aa07bb8277238e5a6847d7d32
M. Michaud Aurelien <a_michau@insa-10227>
2018-11-12 12:34:44 +0100

    Affiche contenu des pays sur la carte lorsque l'on clique dessus

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -9,16 +9,3 @@ function toggle(div)
 	document.getElementById(div).style.display =  "none";
     }
 }
-
-function toggleMapContent(div)
-{
-    alert("Hello");
-    if(document.getElementById(div).style.display == "none")
-    {
-	document.getElementById(div).style.display =  "block";
-    }
-    else
-    {
-	document.getElementById(div).style.display =  "none";
-    }
-}
