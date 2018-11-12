7f76fdc65b2525f282703c2bd2219f58c94cc199
M. Michaud Aurelien <a_michau@insa-10227>
2018-11-12 13:20:16 +0100

    Change country's color when selected

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -9,3 +9,34 @@ function toggle(div)
 	document.getElementById(div).style.display =  "none";
     }
 }
+
+function toggleMap(div)
+{
+    var transition = {
+	"BRAZIL"	: "BR",
+	"CANADA"	: "CA",
+	"CHINA"		: "CN",
+	"COLOMBIA"	: "CO",
+	"SPAIN"		: "ES",
+	"JAPAN"		: "JP",
+	"USA"		: "US",
+	"SOUTHKOREA"	: "KR",
+	"LEBANON"	: "LB",
+	"NORWAY"	: "NO",
+	"ROMANIA"	: "RO",
+	"VIETNAM"	: "VN",
+	
+    };
+
+    /* Display content blocks and change color of the country*/
+    if(document.getElementById(div).style.display == "none")
+    {
+	document.getElementById(div).style.display		= "block";
+	document.getElementById(transition[div]).style.fill	= "#CC3300";
+    }
+    else
+    {
+	document.getElementById(div).style.display =  "none";
+	document.getElementById(transition[div]).style.fill	= "black";
+    }
+}
